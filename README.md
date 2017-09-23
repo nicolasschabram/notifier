f5fdf0c5faa259e69a951d957015ccefc31a22c4
Nicolas Schabram <mail@nicolasschabram.de>
2017-09-23 18:10:38 +0200

    better demo notifications

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Setup
 
-* `npm install`
-* `npm start`
-* `npm test`
+* Get repository from Github: `git clone https://github.com/nicolasschabram/notifier`
+* Install React app: `npm install`
+* Start development server: `npm start`
+* Access demo on http://localhost:3000/
+* Run tests and generate test coverage report: `npm test`
--- src/data/notifications/reducer.js
+++ src/data/notifications/reducer.js
@@ -3,26 +3,26 @@ import { fromJS, Map } from "immutable";
 import uid from "uid";
 
 const initialState = fromJS([{
-  id: 'a7fh4ncs8d',
-  header: 'Notification 1',
-  body: 'This is the first info notification.',
-  type: 'info',
+  id: "a7fh4ncs8d",
+  header: "We've received your order.",
+  body: "Dear John, thanks for putting your trust in us. Your order will be processed soon – we'll be in touch.",
+  type: "info",
   isOpen: true,
-  date: new Date(2017, 5, 16, 17, 25)
+  date: new Date(2017, 5, 16, 17, 23)
 }, {
-  id: 'a7fh4ncs8f',
-  header: 'Notification 2',
-  body: 'This is another notification – this time a warning.',
-  type: 'warning',
+  id: "a7fh4ncs8f",
+  header: "Please check your payment details!",
+  body: "Hi John. There seems to be a problem with your credit card. We need you to provide us with a valid payment method, otherwise we'll have to cancel the order.",
+  type: "warning",
   isOpen: true,
   date: new Date(2017, 5, 16, 17, 24)
 }, {
   id: 'a7fh4ncf8z',
-  header: 'Notification 3',
-  body: 'And this is an error notification. It has a very long body message and might span multiple lines. The layout should know how to handle this.',
-  type: 'error',
+  header: "Your order has been cancelled.",
+  body: "Hello John! It seems like we can't validate your payment details. Therefore, we were forced to cancel your order. Please check your credit card info and then make a new order.",
+  type: "error",
   isOpen: true,
-  date: new Date(2017, 5, 16, 17, 23)
+  date: new Date(2017, 5, 16, 17, 25)
 }]);
 
 export function reducer(state=initialState, action) {
